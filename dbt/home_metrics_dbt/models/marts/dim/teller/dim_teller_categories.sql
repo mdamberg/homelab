0876e056e0b8611171fdede4ce5ed9d1b9c6eb47
DataDad <mattdamberg@gmail.com>
2026-06-20 11:23:41 -0400

    added missing transaction categories to trans mapping file. Added dim teller categories sql;

--- dbt/home_metrics_dbt/models/marts/dim/teller/dim_teller_categories.sql
+++ dbt/home_metrics_dbt/models/marts/dim/teller/dim_teller_categories.sql
@@ -4,19 +4,9 @@
     enabled = false
 ) }}
 
-
 select
-    select
-    account_pk,
-    account_key,
-    account_holder,
-    account_holder_key,
-    account_name_friendly,
-    teller_account_id,
-    account_type,
-    account_subtype,
-    last_four,
-    account_status,
-    institution_name,
-    institution_id,
-    enrollment_id
+    category_key,
+    category,
+    subcategory,
+    teller_category,
+from {{ ref('intmdt_teller_transactions') }}
