0a5735883bbc4d2021d299fa1108acd9a31fdf35
DataDad <mattdamberg@gmail.com>
2026-09-19 18:44:41 -0400

    added new flags to yaml

--- dbt/home_metrics_dbt/models/marts/hardware/homelab_hardware_sensor.sql
+++ dbt/home_metrics_dbt/models/marts/hardware/homelab_hardware_sensor.sql
@@ -6,6 +6,6 @@
 select  
     {{ dbt_utils.generate_surrogate_key(['recorded_datetime', 'sensor_id']) }} as hardware_skey,
     *,
-    case when date_trunc(month, recorded_date) = date_trunc(month, current_date) then 1 else 0 end as is_current_month,
+    case when date_trunc('month', recorded_date) = date_trunc('month', current_date) then 1 else 0 end as is_current_month,
     case when sensor_name ilike '%temp%' then 1 else 0 end as is_temp_sensor
 from  {{ ref('fct_hardware_sensor') }}
