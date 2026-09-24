9683d4270c340754606feb0e6790bb46f0cac994
DataDad <mattdamberg@gmail.com>
2026-09-23 20:39:48 -0400

    added date filters to dim_date

--- dbt/home_metrics_dbt/models/dim/dates/dim_date.sql
+++ dbt/home_metrics_dbt/models/dim/dates/dim_date.sql
@@ -44,3 +44,5 @@ SELECT
     case when is_bom = true then 1 else 0 end as is_bom_flag,
     case when ABS((week_start_date - '2025-03-07'::date) / 7) % 2 = 0 then 1 else 0 end as is_payweek
 FROM dim_date_cte
+where month_start_date <= current_date
+and month_start_date >= '2026-01-01'
